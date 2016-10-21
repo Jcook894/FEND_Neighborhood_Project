ff08d7d0d922507ac050cb3a11c895fd10aae7be
Jcook894 <Jcook894@icloud.com>
2016-10-21 15:58:22 -0400

    Changed color of marker when click, need to fix minor problem.

--- js/app.js
+++ js/app.js
@@ -1,13 +1,29 @@
+<!-- Global Variables -->
+var map;
+var marker;
+var locationArray = ko.observableArray ([
+  {title:'Hartford Hospital', location:{lat: 41.754582, lng:-72.678633} },
+  {title:'School', location:{lat: 41.755042, lng:-72.665532} },
+  {title:'Park', location:{lat: 41.757419, lng:-72.664175} },
+  {title:'House', location:{lat: 41.764117, lng:-72.671873} },
+  {title:'Condo', location:{lat: 41.767228, lng:-72.676470} }
+]);
+
+var style=[{elementType:"geometry",stylers:[{color:"#1d2c4d"}]},{elementType:"labels.text.fill",stylers:[{color:"#8ec3b9"}]},{elementType:"labels.text.stroke",stylers:[{color:"#1a3646"}]},{featureType:"administrative.country",elementType:"geometry.stroke",stylers:[{color:"#4b6878"}]},{featureType:"administrative.land_parcel",elementType:"labels.text.fill",stylers:[{color:"#64779e"}]},{featureType:"administrative.province",elementType:"geometry.stroke",stylers:[{color:"#4b6878"}]},{featureType:"landscape.man_made",elementType:"geometry.stroke",stylers:[{color:"#334e87"}]},{featureType:"landscape.natural",elementType:"geometry",stylers:[{color:"#023e58"}]},{featureType:"poi",elementType:"geometry",stylers:[{color:"#283d6a"}]},{featureType:"poi",elementType:"labels.text.fill",stylers:[{color:"#6f9ba5"}]},{featureType:"poi",elementType:"labels.text.stroke",stylers:[{color:"#1d2c4d"}]},{featureType:"poi.park",elementType:"geometry.fill",stylers:[{color:"#023e58"}]},{featureType:"poi.park",elementType:"labels.text.fill",stylers:[{color:"#3C7680"}]},{featureType:"road",elementType:"geometry",stylers:[{color:"#304a7d"}]},{featureType:"road",elementType:"labels.text.fill",stylers:[{color:"#98a5be"}]},{featureType:"road",elementType:"labels.text.stroke",stylers:[{color:"#1d2c4d"}]},{featureType:"road.highway",elementType:"geometry",stylers:[{color:"#2c6675"}]},{featureType:"road.highway",elementType:"geometry.stroke",stylers:[{color:"#255763"}]},{featureType:"road.highway",elementType:"labels.text.fill",stylers:[{color:"#b0d5ce"}]},{featureType:"road.highway",elementType:"labels.text.stroke",stylers:[{color:"#023e58"}]},{featureType:"transit",elementType:"labels.text.fill",stylers:[{color:"#98a5be"}]},{featureType:"transit",elementType:"labels.text.stroke",stylers:[{color:"#1d2c4d"}]},{featureType:"transit.line",elementType:"geometry.fill",stylers:[{color:"#283d6a"}]},{featureType:"transit.station",elementType:"geometry",stylers:[{color:"#3a4762"}]},{featureType:"water",elementType:"geometry",stylers:[{color:"#0e1626"}]},{featureType:"water",elementType:"labels.text.fill",stylers:[{color:"#4e6d70"}]}];
+
+
 
 <!-- GoogleMap API -->
 function initMap() {
-
 
   map = new google.maps.Map(document.getElementById('map'), {
     center: {lat: 41.764117, lng: -72.676470},
     zoom: 13,
     styles: style
   });
+
+  var defaultMarker = changeMarker('0091ff');
+  var highlightedMarker = changeMarker('ff66ff');
 
 // Iterates through the locationArray and gives the marker a proper
 // location.
@@ -31,12 +47,15 @@ function initMap() {
         position: locations
       });
 
-
-
+      marker.addListener('click', function() {
+            this.setIcon(highlightedMarker);
+          });
+          marker.addListener('click', function() {
+            this.setIcon(defaultMarker);
+          });
 
       google.maps.event.addListener(marker,'click', ( function(marker){
           return function() {
-          
           infoWindow.setContent("<div>" + marker.title + "</div>");
           infoWindow.open( map, marker);
       }
@@ -45,34 +64,20 @@ function initMap() {
 
 };
 
-function windowInfo(marker, infoWindow){
+function changeMarker (color){
+  var markerColor = new google.maps.MarkerImage(
+    'http://chart.googleapis.com/chart?chst=d_map_spin&chld=1.15|0|'+ color +
+          '|40|_|%E2%80%A2',
 
-  if(infoWindow.marker == marker){
-    infoWindow.setContent('');
-    infoWindow.marker = marker;
+          new google.maps.Size(21, 34),
+          new google.maps.Point(0, 0),
+          new google.maps.Point(10, 34),
+          new google.maps.Size(21,34));
+        return markerColor;
 
-    infowindow.addListener('closeclick', function() {
-      infowindow.marker = null;
-    });
-  }
 }
 
 };
-
-
-
-<!-- Global Variables -->
-var map;
-var marker;
-var locationArray = ko.observableArray ([
-  {title:'Hartford Hospital', location:{lat: 41.754582, lng:-72.678633} },
-  {title:'School', location:{lat: 41.755042, lng:-72.665532} },
-  {title:'Park', location:{lat: 41.757419, lng:-72.664175} },
-  {title:'House', location:{lat: 41.764117, lng:-72.671873} },
-  {title:'Condo', location:{lat: 41.767228, lng:-72.676470} }
-]);
-
-var style=[{elementType:"geometry",stylers:[{color:"#1d2c4d"}]},{elementType:"labels.text.fill",stylers:[{color:"#8ec3b9"}]},{elementType:"labels.text.stroke",stylers:[{color:"#1a3646"}]},{featureType:"administrative.country",elementType:"geometry.stroke",stylers:[{color:"#4b6878"}]},{featureType:"administrative.land_parcel",elementType:"labels.text.fill",stylers:[{color:"#64779e"}]},{featureType:"administrative.province",elementType:"geometry.stroke",stylers:[{color:"#4b6878"}]},{featureType:"landscape.man_made",elementType:"geometry.stroke",stylers:[{color:"#334e87"}]},{featureType:"landscape.natural",elementType:"geometry",stylers:[{color:"#023e58"}]},{featureType:"poi",elementType:"geometry",stylers:[{color:"#283d6a"}]},{featureType:"poi",elementType:"labels.text.fill",stylers:[{color:"#6f9ba5"}]},{featureType:"poi",elementType:"labels.text.stroke",stylers:[{color:"#1d2c4d"}]},{featureType:"poi.park",elementType:"geometry.fill",stylers:[{color:"#023e58"}]},{featureType:"poi.park",elementType:"labels.text.fill",stylers:[{color:"#3C7680"}]},{featureType:"road",elementType:"geometry",stylers:[{color:"#304a7d"}]},{featureType:"road",elementType:"labels.text.fill",stylers:[{color:"#98a5be"}]},{featureType:"road",elementType:"labels.text.stroke",stylers:[{color:"#1d2c4d"}]},{featureType:"road.highway",elementType:"geometry",stylers:[{color:"#2c6675"}]},{featureType:"road.highway",elementType:"geometry.stroke",stylers:[{color:"#255763"}]},{featureType:"road.highway",elementType:"labels.text.fill",stylers:[{color:"#b0d5ce"}]},{featureType:"road.highway",elementType:"labels.text.stroke",stylers:[{color:"#023e58"}]},{featureType:"transit",elementType:"labels.text.fill",stylers:[{color:"#98a5be"}]},{featureType:"transit",elementType:"labels.text.stroke",stylers:[{color:"#1d2c4d"}]},{featureType:"transit.line",elementType:"geometry.fill",stylers:[{color:"#283d6a"}]},{featureType:"transit.station",elementType:"geometry",stylers:[{color:"#3a4762"}]},{featureType:"water",elementType:"geometry",stylers:[{color:"#0e1626"}]},{featureType:"water",elementType:"labels.text.fill",stylers:[{color:"#4e6d70"}]}];
 
 
 <!-- View Model -->
@@ -87,9 +92,9 @@ function viewModel() {
 function openNav() {
     document.getElementById("mySidenav").style.width = "250px";
 };
-
 function closeNav() {
     document.getElementById("mySidenav").style.width = "0";
 };
+
 
 ko.applyBindings( new viewModel());
